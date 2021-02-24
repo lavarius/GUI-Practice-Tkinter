6465ce23275fdc2a540faa9099604545dfba0ddf
Mark Bartolo <mdbartolo@gmail.com>
2021-02-23 19:46:16 -0800

    Orienting Widgets
    Made two frames to split them into two and placed labels, entry, and buttons in each.

--- 1.FirstApp/FirstApp.py
+++ 1.FirstApp/FirstApp.py
@@ -12,33 +12,38 @@ root= tk.Tk() #creating a TK object, main window of application | container
 
 # Create TkStringVar
 user_name = tk.StringVar()
-# Create entry field for the main window
 
-# label 
-name_label = ttk.Label(root, text="Name: ").pack(side = "left", padx=(10, 10)) 
+# Frame to customize orientation of widgets
+input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
+input_frame.pack(fill="both")
+
+
+
+# Create entry field for the main window, label 
+name_label = ttk.Label(input_frame, text="Name: ").pack(side = "left", padx=(10, 10)) 
 # pass container with Label
 # Label in window
 # .pack() layout algorithm puts element into the window
 
-
 # Entry field
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
 # textvariable must have a tk string var
 name_entry.pack(side="left")
 name_entry.focus() # focus to this entry field when window is opened
 
 # Button widgets
-greet_button = ttk.Button(root, text="Greet", command=greet)
+# Button frame for widget formation
+buttons_frame = ttk.Frame(root, padding=(20,10))
+buttons_frame.pack(fill="both")
 
-# pack into the window
-# oriented to the left
-# fill property reserves space
+greet_button = ttk.Button(buttons_frame, text="Greet", command=greet)
+# pack into the window, oriented to the left, fill property reserves space
 # expand, fills extra space by expanding
-greet_button.pack(side="left", fill="both", expand=True)
+greet_button.pack(side="left", fill="x", expand=True)
 
 # quit buttonn
-quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+quit_button = ttk.Button(buttons_frame, text="Quit", command=root.destroy)
 #pack into the window
-quit_button.pack(side="left", fill="x")
+quit_button.pack(side="left", fill="x", expand=True)
 
 root.mainloop() # starts running
