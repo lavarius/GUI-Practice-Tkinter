2cc7874928565df4113e7fd33288003d697f897b
Mark Bartolo <mdbartolo@gmail.com>
2021-02-23 20:10:32 -0800

    Working with Grid Geometry
    Covered grid geometry and parameters of row column and sticky.

--- 1.FirstApp/FirstApp.py
+++ 1.FirstApp/FirstApp.py
@@ -12,35 +12,43 @@ root= tk.Tk() #creating a TK object, main window of application | container
 # title for the root window
 root.title("Greeter App")
 
+# configure column sizing aswindow changes
+root.columnconfigure(0,  weight=1)
+
 # Create TkStringVar
 user_name = tk.StringVar()
 
 # Frame to customize orientation of widgets
 input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
-input_frame.pack(fill="both")
+# Grid Geoemetry
+input_frame.grid(row=0, column=0, sticky="EW")
 
 # Create entry field for the main window, label 
 name_label = ttk.Label(input_frame, text="Name: ") # pass container with Label
 # Label in window, .pack() layout algorithm puts element into the window
-name_label.pack(side = "left", padx=(10, 10)) 
+name_label.grid(row=0, column= 0, padx=(10, 10)) 
 
 # Entry field
 name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
 # textvariable must have a tk string var
-name_entry.pack(side="left")
+name_entry.grid(row=0, column= 1)
 name_entry.focus() # focus to this entry field when window is opened
 
 # Button widgets
 #   Button frame for widget formation
 buttons_frame = ttk.Frame(root, padding=(20,10))
-buttons_frame.pack(fill="both")
+buttons_frame.grid(row=1, column=0, sticky="EW") # stick east and west
+
+# size changing buttons using tuples
+buttons_frame.columnconfigure(0,  weight=1)
+buttons_frame.columnconfigure(1,  weight=1)
 
 greet_button = ttk.Button(buttons_frame, text="Greet", command=greet)
-greet_button.pack(side="left", fill="x", expand=True)
+greet_button.grid(row=0, column= 0, sticky="EW")
 
 # quit buttonn
 quit_button = ttk.Button(buttons_frame, text="Quit", command=root.destroy)
 #pack into the window
-quit_button.pack(side="left", fill="x", expand=True)
+quit_button.grid(row=0, column= 1, sticky="EW")
 
-root.mainloop() # starts running
+root.mainloop() # starts running window and pauses the program here
