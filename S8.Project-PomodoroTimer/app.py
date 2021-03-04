1f27f2ae451732261b288b9a91d9d3d28c5efafd
Mark Bartolo <mdbartolo@gmail.com>
2021-03-03 18:41:43 -0800

    Splitting app into multiple files
    Rearranged files, renamed files, included __init__.py file and separated files - app file and the timer file in frames directory

---
+++ S8.Project-PomodoroTimer/app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 2 13:48 2021
+Milestone Project: Pomodoro Timer
+app.py
+Basically main
+
+@author: lavarius
+"""
+import tkinter as tk
+from tkinter import ttk
+from collections import deque # for rotating items in a list
+from frames import Timer #, Settings
+# controller
+class PomodoroTimer(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title("Pomodoro Timer")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
+        # creating amount of time each timer lasts for
+        self.pomodoro = tk.StringVar(value=25)
+        self.long_break = tk.StringVar(value=15)
+        self.short_break = tk.StringVar(value=5)
+
+        # control time and timings, timer moved from timer class
+        self.timer_order = ["Pomodoro", "Short Break", "Pomodoro", "Short Break", "Pomodoro", "long Break"]
+        # Create a deck with this list, keeping it unchanged, Rearranging with deck instead of a list
+        self.timer_schedule = deque(self.timer_order) # rotate elements from one end to another
+
+        container = ttk.Frame(self)
+        container.grid()
+        container.columnconfigure(0, weight=1)
+
+        timer_frame = Timer(container, self)
+        timer_frame.grid(row=0, column=0, sticky="NESW")
+
+app = PomodoroTimer()
+app.mainloop()
--- S8.Project-PomodoroTimer/frames/timer.py
+++ S8.Project-PomodoroTimer/frames/timer.py
@@ -3,36 +3,13 @@
 """
 Created on Tue Mar 2 13:48 2021
 Milestone Project: Pomodoro Timer
+timer.py
+Keeps track of timers and timing
 @author: lavarius
 """
 import tkinter as tk
 from tkinter import ttk
 from collections import deque # for rotating items in a list
-# controller
-class PomodoroTimer(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.title("Pomodoro Timer")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-
-        # creating amount of time each timer lasts for
-        self.pomodoro = tk.StringVar(value=25)
-        self.long_break = tk.StringVar(value=15)
-        self.short_break = tk.StringVar(value=5)
-
-        # control time and timings, timer moved from timer class
-        self.timer_order = ["Pomodoro", "Short Break", "Pomodoro", "Short Break", "Pomodoro", "long Break"]
-        # Create a deck with this list, keeping it unchanged, Rearranging with deck instead of a list
-        self.timer_schedule = deque(self.timer_order) # rotate elements from one end to another
-
-        container = ttk.Frame(self)
-        container.grid()
-        container.columnconfigure(0, weight=1)
-
-        timer_frame = Timer(container, self)
-        timer_frame.grid(row=0, column=0, sticky="NESW")
 
 # only control timer
 class Timer(ttk.Frame):
@@ -40,7 +17,7 @@ class Timer(ttk.Frame):
         super().__init__(parent)
         self.controller = controller
         pomodoro_time = int(controller.pomodoro.get())
-        self.current_time = tk.StringVar(value=f"{pomodoro_time:02d}:10")
+        self.current_time = tk.StringVar(value=f"{pomodoro_time:02d}:00")
         # creating a label to show which timer we are on
         self.current_timer_label = tk.StringVar(value=controller.timer_schedule[0])
         self.timer_running = False
@@ -156,7 +133,3 @@ class Timer(ttk.Frame):
                 self.current_time.set(f"{long_break_time:02d}:00")
                 
             self.after(1000, self.decrement_time)
-
-
-app = PomodoroTimer()
-app.mainloop()
